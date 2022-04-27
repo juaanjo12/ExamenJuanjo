7ab3181da34e8c6d1baf75d3ace63ba8fd98a298
usuario <juanjocantero0123@gmail.com>
2022-04-27 09:22:30 +0200

    añadimos suma

---
+++ prueba1/src/prueba1/Suma.java
@@ -0,0 +1,20 @@
+package prueba1;
+
+import javax.swing.JOptionPane;
+
+public class Suma {
+
+	public static void main(String[] args) {
+
+		String str = JOptionPane.showInputDialog("Introduzca un número");
+		int var1 = Integer.parseInt(str); //Pido un número 
+		
+		str = JOptionPane.showInputDialog("Introduzca un número");
+		int var2 = Integer.parseInt(str); //Pido un segundo número
+		
+		int suma = var1 + var2;
+		System.out.println("Suma de los números introducidos = " + suma);
+		
+	}
+
+}
